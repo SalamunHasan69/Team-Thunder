5d7dd4ca3886c569541193d0b8d1acd67d4edeb0
SalamunHasan69e <salamun2600@gmail.com>
2022-09-24 17:52:04 +0600

    second commit

--- src/component/Players/Players.js
+++ src/component/Players/Players.js
@@ -12,7 +12,7 @@ const Players = () => {
   }, [])
   console.log(players);
   return (
-    <div className='lg:grid grid-cols-3 mt-8 max-w-[1300px] mx-auto items-center	'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-8 max-w-[1300px] mx-auto items-center	'>
       {
         players.map(xyz => <Player xyz={xyz} key={xyz.id}></Player>)
       }
